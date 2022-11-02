9a1cbcefa42c601461d51b239da314002033182b
Parkle Lee <parkle.lee@xenyo.com>
2022-11-02 13:58:36 +0800

    Update webhooks scripts

--- webhooks.js
+++ webhooks.js
@@ -3,18 +3,21 @@ const { queue, server, webhooks, logger } = require('github-webhooks-exec');
 webhooks.on('push', event => {
   logger.info(`${event.payload.ref} was updated in ${event.payload.repository.full_name}.`);
   const commands = [
+    'set -ex',
     'vendor/bin/drush cex -y',
+    'git reset',
     'git add config',
-    'git commit -m "Export active configuration"',
+    'git commit -m "Export active configuration" || true',
     'git pull --rebase --autostash',
     'git push',
     'composer install',
     'vendor/bin/drush cr',
     'vendor/bin/drush cim -y',
+    'vendor/bin/drush cr',
     'pnpm install',
-    'pm2 reload ecosystem.config.js',
+    'pnpm reload',
   ];
-  queue.push(commands.join(' && '));
+  queue.push(commands.join('; '));
 });
 
 server.listen(process.env.GITHUB_WEBHOOKS_PORT);
